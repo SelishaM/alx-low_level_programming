2520e46c51b2b99d20a95f50c1c1856a55314667
SelishaM <selishamoodley06@gmail.com>
2022-06-07 11:34:51 -0700

    Task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,9 +4,9 @@
  * add - add two numbers from input
  * @a: first parameter
  * @b: second parameter
- *
+ * add a and b
  * Description: add two numbers
- * Result: Always (0).
+ * Result: Always (0). result to add
  */
 
 int add(int a, int b)
