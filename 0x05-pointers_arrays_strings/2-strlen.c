6540feec86b652f40c95a33ab6114d9f22d26683
SelishaM <selishamoodley06@gmail.com>
2022-06-13 06:49:04 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of the string
+ * @s: Pointer input s
+ * Return: i
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{}
+	return (i);
+}
